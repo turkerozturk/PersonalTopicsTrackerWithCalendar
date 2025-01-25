5839d5e8b7c641962b956709082ab2238e953483
turkerozturk <turker_ozt@yahoo.com>
2025-01-26 01:57:10 +0300

    Filter topics by contained text

--- src/main/java/com/turkerozturk/App.java
+++ src/main/java/com/turkerozturk/App.java
@@ -28,10 +28,14 @@ public class App {
     private JLabel monthLabel;
 
     private JLabel cellContentLabel;
+
+    JTextField searchField;
     private LocalDate currentDate;
     private Map<String, Integer> topicTotals;
     private DefaultTableModel tableModel;
     private List<String> topics;
+
+    TableRowSorter<DefaultTableModel> sorter;
 
     private Connection connection;
 
@@ -51,26 +55,42 @@ public class App {
             topicTotals.put(topic, 0);
         }
 
-        cellContentLabel = new JLabel(bundle.getString("message.hover.over.a.cell"));
-        JPanel topPanel = new JPanel(new BorderLayout());
+
+        // Ana panel oluştur ve BoxLayout kullan
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS)); // Yatay eksende sıralama
+
+        JPanel navigationPanel = new JPanel(new BorderLayout());
+        //navigationPanel.setBackground(Color.RED);
+
+        cellContentLabel = new JLabel(bundle.getString("message.hover.over.a.cell"), SwingConstants.CENTER);
+
+        JPanel cellContentPanel = new JPanel(new BorderLayout());
+        cellContentPanel.add(cellContentLabel, BorderLayout.CENTER);
+        //cellContentPanel.setBackground(Color.BLUE);
+
         JButton prevMonthButton = new JButton(bundle.getString("button.previous.month"));
         JButton nextMonthButton = new JButton(bundle.getString("button.next.month"));
         JButton manageTopicsButton = new JButton(bundle.getString("button.topic.management"));
         monthLabel = new JLabel("", SwingConstants.CENTER);
-        updateMonthLabel();
+        String formattedDate = updateMonthLabel();
+        monthLabel.setText(formattedDate);
 
         prevMonthButton.addActionListener(e -> changeMonth(-1));
         nextMonthButton.addActionListener(e -> changeMonth(1));
         manageTopicsButton.addActionListener(e -> manageTopics());
 
-        topPanel.add(prevMonthButton, BorderLayout.WEST);
-        topPanel.add(monthLabel, BorderLayout.CENTER);
+        navigationPanel.add(prevMonthButton, BorderLayout.WEST);
+        navigationPanel.add(monthLabel, BorderLayout.CENTER);
 
-        topPanel.add(nextMonthButton, BorderLayout.EAST);
-        topPanel.add(manageTopicsButton, BorderLayout.NORTH);
+        navigationPanel.add(nextMonthButton, BorderLayout.EAST);
+        navigationPanel.add(manageTopicsButton, BorderLayout.NORTH);
 
-        JPanel topPanel2 = new JPanel(new BorderLayout());
-        topPanel.add(cellContentLabel, BorderLayout.SOUTH);
+        JPanel searchPanel = new JPanel(new BorderLayout());
+        //searchPanel.setBackground(Color.GREEN);
+        JLabel searchLabel = new JLabel("Filter Topic Text: ", SwingConstants.LEFT);
+        searchPanel.add(searchLabel, BorderLayout.WEST);
+
 
 
         tableModel = new DefaultTableModel();
@@ -105,8 +125,30 @@ public class App {
         };
 
         // Sıralayıcı ekleniyor
-        TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(tableModel);
+        sorter = new TableRowSorter<>(tableModel);
         table.setRowSorter(sorter);
+
+       // topPanel2.add(new JLabel("Search: "), BorderLayout.WEST);
+
+        // Arama alanı
+        searchField = new JTextField(20);
+        searchPanel.add(searchField, BorderLayout.CENTER);
+
+        searchField.getDocument().addDocumentListener(new javax.swing.event.DocumentListener() {
+            public void insertUpdate(javax.swing.event.DocumentEvent e) {
+                filterTable();
+            }
+
+            public void removeUpdate(javax.swing.event.DocumentEvent e) {
+                filterTable();
+            }
+
+            public void changedUpdate(javax.swing.event.DocumentEvent e) {
+                filterTable();
+            }
+
+
+        });
 
         table.addMouseListener(new MouseAdapter() {
             @Override
@@ -134,8 +176,8 @@ public class App {
         });
 
 
-        cellContentLabel.setBounds(10, 300, 300, 30);
-        cellContentLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        //cellContentLabel.setBounds(10, 300, 300, 30);
+        //cellContentLabel.setHorizontalAlignment(SwingConstants.LEFT);
 
         table.addMouseMotionListener(new MouseMotionAdapter() {
             public void mouseMoved(MouseEvent e) {
@@ -154,15 +196,40 @@ public class App {
             }
         });
 
+
+
         JScrollPane scrollPane = new JScrollPane(table);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        frame.setLayout(new BorderLayout());
-        frame.add(topPanel, BorderLayout.NORTH);
-        frame.add(scrollPane, BorderLayout.CENTER);
+        //frame.setLayout(new BorderLayout());
+
+        mainPanel.add(navigationPanel);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Araya boşluk eklemek için
+        mainPanel.add(searchPanel);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Araya boşluk eklemek için
+        mainPanel.add(cellContentPanel);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Araya boşluk eklemek için
+        mainPanel.add(scrollPane);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Araya boşluk eklemek için
+
+
+        frame.add(mainPanel);
+
+        //frame.add(topPanel2, BorderLayout.NORTH);
+
+        //frame.add(scrollPane);
 
         updateTable();
 
         frame.setVisible(true);
+    }
+
+    private void filterTable() {
+        String searchText = searchField.getText();
+        if (searchText.trim().isEmpty()) {
+            sorter.setRowFilter(null); // Filtreyi kaldır
+        } else {
+            sorter.setRowFilter(RowFilter.regexFilter("(?i)" + searchText, 0)); // İlk sütunda arama
+        }
     }
 
     private void setupDatabase() {
@@ -192,6 +259,7 @@ public class App {
         }
     }
 
+    // db
     private List<String> loadTopics() {
         List<String> loadedTopics = new ArrayList<>();
         try {
@@ -206,20 +274,21 @@ public class App {
         return loadedTopics;
     }
 
-    private void updateMonthLabel() {
+    private String updateMonthLabel() {
         String localeCode = bundle.getString("app.locale");
         Locale locale = new Locale(localeCode);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy", locale);
         String formattedDate = currentDate.format(formatter);
-        monthLabel.setText(formattedDate);
+        return formattedDate;
     }
 
     private void changeMonth(int delta) {
         currentDate = currentDate.plusMonths(delta);
-        updateMonthLabel();
+        String formattedDate = updateMonthLabel();
+        monthLabel.setText(formattedDate);
         updateTable();
-
+        filterTable();
     }
 
     private void updateTable() {
@@ -311,6 +380,7 @@ public class App {
     }
 
 
+    // db
     private String updateWhenLast(String topic) {
         String whenLastValue = "N/A"; // Varsayılan değer
 
@@ -350,6 +420,7 @@ public class App {
 
 
 
+    // db
     private long getTopicIdByName(String topicName) {
         try {
             PreparedStatement ps = connection.prepareStatement(
@@ -366,6 +437,7 @@ public class App {
         return -1; // Hata durumunda geçersiz bir değer döndür
     }
 
+    // db
     private int getCellStatus(String topic, String date) {
         try {
             PreparedStatement ps = connection.prepareStatement("SELECT is_marked FROM activity_log WHERE topic_id = (SELECT id FROM topics WHERE name = ?) AND date = ?");
@@ -381,6 +453,7 @@ public class App {
         return 0;
     }
 
+    // db
     private void toggleCellStatus(String topic, String date) {
         long currentDateAsEpoch = Instant.now().toEpochMilli();
         try {
@@ -554,7 +627,7 @@ public class App {
 
 
 
-
+    // db
     private void updateCellContent(long topicId, String date, String content) {
         long currentDateAsEpoch = Instant.now().toEpochMilli();
 
